a08602348f9c39a24a907557987b03b91caa133d
Alejo <alejodfr@gmail.com>
2025-03-06 12:40:10 -0600

    Added money field to the Player class

--- Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Player.kt
+++ Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Player.kt
@@ -2,7 +2,7 @@ package com.dallascollege.monopoly.model
 
 class Player(val name: String) {
     var position: Int = 0
-    var balance: Int = 1500
+    var money: Int = 1500
 
     fun move(steps: Int, board: Board) {
         position = (position + steps) % board.tiles.size
