8e94609b6e89a5c9d5b920fc5a547e282dfd1446
Jenniffer Montoya <81304318+JenniMont@users.noreply.github.com>
2025-05-08 12:17:47 -0500

    Remove unnecessary getter and setter methods from models

--- Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Action.kt
+++ Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Action.kt
@@ -3,46 +3,10 @@ package com.dallascollege.monopoly.model
 import com.dallascollege.monopoly.enums.ActionType
 
 class Action(
-    name: String,
-    description: String = "",
-    actionType: ActionType,
-    propertyId: Int = -1,
-    money: Int = 0,
-    quantity: Int = 0
-) {
-    var name: String = name
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var description: String = description
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var actionType: ActionType = actionType
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var propertyId: Int = propertyId
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var money: Int = money
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var quantity: Int = quantity
-        get() = field
-        set(value) {
-            field = value
-        }
-}
+   val name: String,
+   val description: String = "",
+   val actionType: ActionType,
+   var propertyId: Int = -1,
+   var money: Int = 0,
+   var quantity: Int = 0
+)
--- Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Property.kt
+++ Monopoly/composeApp/src/desktopMain/kotlin/com/dallascollege/monopoly/model/Property.kt
@@ -3,83 +3,18 @@ package com.dallascollege.monopoly.model
 import com.dallascollege.monopoly.enums.PropertyColor
 
 class Property(
-    id: Int,
-    name: String,
-    price: Int,
-    color: PropertyColor = PropertyColor.WHITE,
-    isPurchased: Boolean= false,
-    baseRent: Int = 0,
-    isUtility: Boolean = false,
-    isRailRoad: Boolean = false,
-    numHouses: Int = 0,
-    numHotels: Int = 0,
-    isMortgaged: Boolean = false
+    var id: Int,
+    var name: String,
+    var price: Int,
+    var color: PropertyColor = PropertyColor.WHITE,
+    var isPurchased: Boolean= false,
+    var baseRent: Int = 0,
+    var isUtility: Boolean = false,
+    var isRailRoad: Boolean = false,
+    var numHouses: Int = 0,
+    var numHotels: Int = 0,
+    var isMortgaged: Boolean = false
 ) {
-    var id: Int = id
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var name: String = name
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var price: Int = price
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var color: PropertyColor = color
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var isPurchased: Boolean = isPurchased
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var baseRent: Int = baseRent
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var isUtility: Boolean = isUtility
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var isRailRoad: Boolean = isRailRoad
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var numHouses: Int = numHouses
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var numHotels: Int = numHotels
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    var isMortgaged: Boolean = isMortgaged
-        get() = field
-        set(value) {
-            field = value
-        }
 
     fun isMortgageable(): Boolean {
         return !isMortgaged && numHotels == 0 && numHouses == 0;
